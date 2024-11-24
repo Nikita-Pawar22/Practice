b452f0b79cb85d1d74544240eb233d8e94af17db
Nikita-Pawar22 <159059041+Nikita-Pawar22@users.noreply.github.com>
2024-11-24 22:31:27 +0530

    Update TicTacToe.c

--- TicTacToe.c
+++ TicTacToe.c
@@ -2,177 +2,86 @@
 
 char board[3][3];
 
-void initializeBoard()
-{
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
+void initializeBoard() {
+   
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             board[i][j] = ' ';
         }
     }
-    int count = 1;
-    printf("\n\n\t  ");
-    for(int i = 0; i < 3; i++)
-    {
-        for(int j = 0; j < 3; j++)
-        {
-            printf("%d", count++);
-            if (j < 2)
-            {
-                printf("  |  ");
-            }
-        }
-        if (i < 2)
-            printf("\n\t----------------\n\t  ");
-    }
-    printf("\n\n\n");
 }
 
-void showBoard()
-{
-    printf("\n\n\t  ");
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            printf("%c", board[i][j]);
-            if (j < 2)
-            {
-                printf("  |  ");
-            }
+void printBoard() {
+   
+    printf("\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf(" %c ", board[i][j]);
+            if (j < 2) printf("|");
         }
-        if (i < 2)
-            printf("\n\t----------------\n\t  ");
+        printf("\n");
+        if (i < 2) printf("---+---+---\n");
     }
-    printf("\n\n\n");
+    printf("\n");
 }
 
-int updateBoard(int cell, char playerSign)
-{
-    int row = (cell - 1) / 3;
-    int col = (cell - 1) % 3;
+int checkWinner() {
 
-    if (board[row][col] != ' ')
-    {
-        printf("\nInvalid: Cell is already filled!\n");
-        return 0;
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
+            return board[i][0] == 'X' ? 1 : -1;
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
+            return board[0][i] == 'X' ? 1 : -1;
     }
-
-    board[row][col] = playerSign;
-    showBoard();
-
-    return 1;
-}
-
-int checkWinner(char sg)
-{
-    for (int i = 0; i < 3; i++)
-    {
-        if ((board[i][0] == sg && board[i][1] == sg && board[i][2] == sg) || // Row
-            (board[0][i] == sg && board[1][i] == sg && board[2][i] == sg)) // Column
-        {
-            return 1;
-        }
-    }
-    if ((board[0][0] == sg && board[1][1] == sg && board[2][2] == sg) ||
-        (board[0][2] == sg && board[1][1] == sg && board[2][0] == sg))
-    {
-        return 1;
-    }
-
+   
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
+        return board[0][0] == 'X' ? 1 : -1;
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
+        return board[0][2] == 'X' ? 1 : -1;
     return 0;
 }
 
-void playTicTacToe()
-{
-    int gameResult = 0;
-    int cell = 0;
-    int playCount = 0;
-    int updationResult = 1;
-
-    char playerSign = ' ';
-
-    while (!gameResult && playCount < 9)
-    {
-        if (playCount % 2 == 0)
-        {
-            printf("\nPlayer 1 [ X ] : ");
-            playerSign = 'X';
-        }
-        else
-        {
-            printf("\nPlayer 2 [ O ] : ");
-            playerSign = 'O';
-        }
-        scanf("%d", &cell);
-        
-        if (cell == -1)
-        {
-            printf("\n\tGame Terminated\n");
-            return;
-        }
-
-        if (cell > 0 && cell < 10)
-        {
-            updationResult = updateBoard(cell, playerSign);
-            if (updationResult)
-            {
-                gameResult = checkWinner(playerSign);
-                if (gameResult)
-                {
-                    printf("\t *** Player %d Won!! ***\n", playerSign == 'X' ? 1 : 2);
-                }
-                playCount++;
-            }
-        }
-        else
-        {
-            printf("\nPlease Enter a valid cell number (1-9)\n");
+int isMovesLeft() {
+   
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == ' ') return 1;
         }
     }
-
-    if (!gameResult && playCount == 9)
-    {
-        printf("\n\t *** Draw...  ***\n");
-    }
-    printf("\n\t --- Game Over --- \n");
+    return 0;
 }
 
-int main()
-{
-    printf("--------- Tic Tac Toe ----------\n\n");
-
-    printf("* Instructions \n\n");
-    printf("\tPlayer 1 sign = X\n");
-    printf("\tPlayer 2 sign = O");
-    printf("\n\tTo exit the game, Enter -1\n");
-
-    printf("\n* Cell Numbers on Board\n");
+void playGame() {
+    
+    int row, col, turn = 1; // 1 for player X, -1 for player O
     initializeBoard();
 
-    char start = ' ';
-    printf("\n> Press Enter to start...");
-    getchar(); 
+    while (1) {
+        printBoard();
+        printf("Player %c, enter your move (row and column): ", (turn == 1) ? 'X' : 'O');
+        scanf("%d %d", &row, &col);
 
-    if (start)
-    {
-        int userChoice = 1;
-        while (userChoice)
-        {
-            playTicTacToe();
-            printf("\n* Menu\n");
-            printf("\nPress 1 to Restart");
-            printf("\nPress 0 to Exit");
-            printf("\n\nChoice: ");
-            scanf("%d", &userChoice);
-            if (userChoice)
-            {
-                initializeBoard();
+        if (board[row][col] == ' ') {
+            board[row][col] = (turn == 1) ? 'X' : 'O';
+            int winner = checkWinner();
+            if (winner != 0) {
+                printBoard();
+                printf("Player %c wins!\n", (winner == 1) ? 'X' : 'O');
+                break;
             }
-            printf("\n");
+            if (!isMovesLeft()) {
+                printBoard();
+                printf("It's a draw!\n");
+                break;
+            }
+            turn = -turn; // Switch turns
+        } else {
+            printf("Invalid move. Try again.\n");
         }
     }
-    printf("\n :: Thanks for playing Tic Tac Toe! :: \n");
+}
+
+int main() {
+    playGame();
     return 0;
 }
